fdcd4acfdbd187189dd950a90feae6abe3730396
tealblu <ckhartsell@gmail.com>
2022-03-09 16:05:09 -0500

    fixed namespaces

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TigerTixWeb
+namespace TigerTix.Web
 {
     public class Program
     {
--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TigerTixWeb
+namespace TigerTix.Web
 {
     public class Startup
     {
