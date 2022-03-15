8b180344b9280d5f2b6a7b95109e9adfed1f4c3c
tealblu <ckhartsell@gmail.com>
2022-03-15 16:09:01 -0400

    added UserRepository.cs and create/delete/read/update funcs

--- Data/UserRepository.cs
+++ Data/UserRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TigerTix.Web.Data.Entities;
 
 namespace TigerTix.Web.Data
 {
@@ -15,6 +16,41 @@ namespace TigerTix.Web.Data
         }
 
         // methods to create, read, update, and delete data in Users table
-        
+        // save a user
+        public void SaveUser(User user)
+        {
+            _context.Add(user);
+            _context.SaveChanges();
+        }
+
+        // returns all users
+        public IEnumerable<User> GetAllUsers()
+        {
+            var users = from u in _context.Users select u;
+
+            return users.ToList();
+        }
+
+        // return a single user by ID
+        public User GetUsersByID(int userID)
+        {
+            var user = (from u in _context.Users where u.Id == userID select u).FirstOrDefault();
+
+            return user;
+        }
+
+        // Update a user
+        public void UpdateUser(User user)
+        {
+            _context.Update(user);
+            _context.SaveChanges();
+        }
+
+        // Delete a user
+        public void DeleteUser(User user)
+        {
+            _context.Remove(user);
+            _context.SaveChanges();
+        }
     }
 }
