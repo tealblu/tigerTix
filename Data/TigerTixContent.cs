08e6ee516be5a09c23c0d6a54c77c3ad2d133470
tealblu <ckhartsell@gmail.com>
2022-03-15 09:37:52 -0400

    database config started

--- Data/TigerTixContent.cs
+++ Data/TigerTixContent.cs
@@ -9,5 +9,19 @@ namespace TigerTix.Web.Data
     public class TigerTixContent : DbContext
     {
         public DbSet<User> Users { get; set; }
+
+        private readonly IConfiguration _config;
+
+        public TigerTixContext (IConfiguration config)
+        {
+            _config = config;
+        }
+
+        proteted override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(_config["ConnectionStrings:DefaultConnection"]);
+        }
     }
 }
