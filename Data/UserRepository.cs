a812032ea9eb661276f1a41163b30ca3d9bcfcff
tealblu <ckhartsell@gmail.com>
2022-03-15 16:04:28 -0400

    started Part VII

---
+++ Data/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TigerTix.Web.Data
+{
+    public class UserRepository
+    {
+        // inject tigertix context and create field to enable access
+        private readonly TigerTixContext _context;
+        public UserRepository(TigerTixContext context)
+        {
+            _context = context;
+        }
+
+        // methods to create, read, update, and delete data in Users table
+        
+    }
+}
